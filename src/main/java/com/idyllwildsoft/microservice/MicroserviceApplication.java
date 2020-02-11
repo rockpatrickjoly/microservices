746674ccfaa808de7f9bd20077dda2bdaab87d61
Patrick Joly <patrict.joly@aircanada.ca>
2020-02-10 20:34:45 -0500

    initial commit

---
+++ src/main/java/com/idyllwildsoft/microservice/MicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.idyllwildsoft.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/idyllwildsoft/microservice/MicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idyllwildsoft.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
